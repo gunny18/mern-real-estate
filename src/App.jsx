4af8e73f63136efae40e3b2a9e58ac6012cdb7ab
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2024-04-01 12:48:06 +0530

    header layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import HeaderLayout from "./components/HeaderLayout";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
+      element: <HeaderLayout />,
       children: [
         { index: true, element: <Home /> },
         { path: "about", element: <About /> },
